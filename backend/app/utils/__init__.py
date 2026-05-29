3e2e3a2073b38e02628ad5675b35fcc623a6ecbf
F25731 <3544524720@qq.com>
2026-05-29 11:40:57 +0800

    Fix utils excel import export

--- backend/app/utils/__init__.py
+++ backend/app/utils/__init__.py
@@ -1,11 +1,13 @@
 from app.utils.link_parser import parse_share_link, build_share_link, normalize_name, parse_tags
 from app.utils.fuzzy_match import name_similarity, tags_overlap, is_fuzzy_duplicate
 from app.utils.security import generate_device_id, generate_api_key, hash_api_key, mask_token
-from app.utils.excel_io import read_excel, write_excel
+from app.utils.excel_io import read_excel_stream, read_csv_stream, read_file_stream, write_excel
+
+read_excel = read_excel_stream
 
 __all__ = [
     "parse_share_link", "build_share_link", "normalize_name", "parse_tags",
     "name_similarity", "tags_overlap", "is_fuzzy_duplicate",
     "generate_device_id", "generate_api_key", "hash_api_key", "mask_token",
-    "read_excel", "write_excel",
+    "read_excel", "read_excel_stream", "read_csv_stream", "read_file_stream", "write_excel",
 ]
